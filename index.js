0f358c36d65dcf235ef2431d6a0670b4890b5134
DamienYule <damienyule@gmail.com>
2021-03-02 11:59:45 -0500

    Only one loop now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //SMART goal 
-    // remove non DRY code
-    // Style into a user friendly app 
-    // reduce to one loop 
+// remove non DRY code
+// Style into a user friendly app 
+// reduce to one loop 
 document.addEventListener('DOMContentLoaded', () => {
     const num = document.querySelector("#number-of-dice")
     const diceOutput = document.querySelector("#dice-para")
@@ -16,25 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
         '&#9860',
         '&#9861',
     ]
-    button.addEventListener('click', () => {        
+    button.addEventListener('click', () => {
         let newArray = []
+        let sum = 0
         for (let i = 0; i < num.value; i++) {
             newArray[i] = arrayOfdice[Math.floor(Math.random() * 6)]
-        }
-        let sum = 0
-        for(let i = 0; i < newArray.length; i++){
-            if (arrayOfdice.includes(newArray[i])){
-            sum += arrayOfdice.indexOf(newArray[i]) + 1 
-            }     
+            if (arrayOfdice.includes(newArray[i])) {
+                sum += arrayOfdice.indexOf(newArray[i]) + 1
+            }
         }
         let newListItem = document.createElement('li')
-        newListItem.innerHTML = `${sum} ${newArray.join('')}` 
+        newListItem.innerHTML = `${sum} ${newArray.join('')}`
         ul.appendChild(newListItem)
-        
+
         sumOutPut.innerHTML = `Sum = ${sum}`
         diceOutput.innerHTML = newArray.join('')
     })
-    
-    
+
+
 })
 
